85e37dca26af1bd636443120f535ae06e111f3fb
miles352 <milestdaly@icloud.com>
2025-01-09 19:50:50 -0500

    Skidded WarriorLost

--- src/main/java/com/example/addon/modules/TrailFollower.java
+++ src/main/java/com/example/addon/modules/TrailFollower.java
@@ -1,27 +1,49 @@
 package com.example.addon.modules;
 
+import baritone.api.BaritoneAPI;
+import baritone.api.pathing.goals.GoalXZ;
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.utils.player.Rotations;
 import meteordevelopment.orbit.EventHandler;
-import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.chunk.WorldChunk;
 import xaeroplus.XaeroPlus;
 import xaeroplus.event.ChunkDataEvent;
 import xaeroplus.module.ModuleManager;
+import xaeroplus.module.impl.OldChunks;
 import xaeroplus.module.impl.PaletteNewChunks;
 
-import static com.example.addon.Utils.posToYaw;
+import java.util.ArrayDeque;
 
 public class TrailFollower extends Module
 {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    public final Setting<FollowModes> chunkLoadMode = sgGeneral.add(new EnumSetting.Builder<FollowModes>()
-        .name("Mode")
-        .description("The mode that chunks are followed")
-        .defaultValue(FollowModes.Basic)
+    public final Setting<Boolean> pitch40 = sgGeneral.add(new BoolSetting.Builder()
+        .name("Auto Pitch 40")
+        .description("Incorporates pitch 40 into the follower.")
+        .defaultValue(true)
+        .build()
+    );
+
+    public final Setting<Integer> maxTrailLength = sgGeneral.add(new IntSetting.Builder()
+        .name("Max Trail Length")
+        .description("The number of trail points to keep for the average. Adjust to change how quickly the average will change.")
+        .defaultValue(20)
+        .min(1)
+        .sliderMax(100)
+        .build()
+    );
+
+    public final Setting<Boolean> pitch40Firework = sgGeneral.add(new BoolSetting.Builder()
+        .name("Auto Firework")
+        .description("Uses a firework automatically if your velocity is too low.")
+        .defaultValue(true)
+        .visible(pitch40::get)
         .build()
     );
 
@@ -32,9 +54,13 @@ public class TrailFollower extends Module
         .build()
     );
 
+    private boolean oldAutoFireworkValue;
+
+    // Credit to WarriorLost: https://github.com/WarriorLost/meteor-client/tree/master
+
     public TrailFollower()
     {
-        super(Addon.CATEGORY, "TrailFollower", "Points you in the direction of newly loaded old chunks.");
+        super(Addon.CATEGORY, "TrailFollower", "Automatically follows trails in all dimensions.");
     }
 
     @Override
@@ -43,7 +69,19 @@ public class TrailFollower extends Module
         XaeroPlus.EVENT_BUS.register(this);
         if (mc.player != null)
         {
-            goalChunkPos = mc.player.getChunkPos();
+            Class<? extends Module> pitch40Util = Pitch40Util.class;
+            Module pitch40UtilModule = Modules.get().get(pitch40Util);
+            if (pitch40.get() && !pitch40UtilModule.isActive())
+            {
+                pitch40UtilModule.toggle();
+                if (pitch40Firework.get())
+                {
+                    Setting<Boolean> setting = ((Setting<Boolean>)pitch40UtilModule.settings.get("Auto Firework"));
+                    oldAutoFireworkValue = setting.get();
+                    setting.set(true);
+                }
+            }
+
             targetYaw = mc.player.getYaw();
         }
         else
@@ -56,72 +94,82 @@ public class TrailFollower extends Module
     public void onDeactivate()
     {
         XaeroPlus.EVENT_BUS.unregister(this);
+        Class<? extends Module> pitch40Util = Pitch40Util.class;
+        Module pitch40UtilModule = Modules.get().get(pitch40Util);
+        if (pitch40.get() && pitch40UtilModule.isActive())
+        {
+            pitch40UtilModule.toggle();
+        }
+        ((Setting<Boolean>)pitch40UtilModule.settings.get("Auto Firework")).set(oldAutoFireworkValue);
     }
 
-    float targetYaw = 16.0f;
+    float targetYaw;
 
     @EventHandler
     private void onTick(TickEvent.Post event)
     {
-        double dYaw = (targetYaw - mc.player.getYaw());
-        dYaw = ((dYaw + 180) % 360) - 180;
-        double yaw = mc.player.getYaw() + dYaw * rotateScaling.get();
-        mc.player.setYaw((float) yaw);
+        mc.player.setYaw(smoothRotation(mc.player.getYaw(), targetYaw));
     }
 
-    ChunkPos goalChunkPos;
-
-
-    private void basicLoad(boolean newChunk, ChunkDataEvent event, boolean seen)
-    {
-        if (seen) return;
-        if (!newChunk &&
-            mc.player.squaredDistanceTo(event.chunk().getPos().getBlockPos(0, 70, 0).toCenterPos()) > mc.player.squaredDistanceTo(goalChunkPos.getBlockPos(0, 70, 0).toCenterPos()))
-        {
-            // Attempt to stay in the direction of the trail by not setting goal to more than 90 degrees off
-            Vec3d pos = mc.player.getPos();
-            Vec3d vec1 = event.chunk().getPos().getCenterAtY((int)pos.y).toCenterPos().subtract(pos).normalize();
-            Vec3d vec2 = goalChunkPos.getCenterAtY((int)pos.y).toCenterPos().subtract(pos).negate().normalize();
-            double dotProduct = vec1.dotProduct(vec2);
-            if (dotProduct > 0) goalChunkPos = event.chunk().getPos();
-        }
-        targetYaw = posToYaw(goalChunkPos.getCenterAtY(70).toCenterPos(), mc);
-    }
-
-
-
-    private void fullyLoad(boolean newChunk, ChunkDataEvent event, boolean seen)
-    {
-        // TODO
-
-    }
+    ArrayDeque<Vec3d> trail = new ArrayDeque<>();
 
     @net.lenni0451.lambdaevents.EventHandler(priority = -1)
     public void onChunkData(ChunkDataEvent event)
     {
-
-        boolean isNewChunk = ModuleManager.getModule(PaletteNewChunks.class)
+        if (event.seenChunk()) return;
+        WorldChunk chunk = event.chunk();
+        boolean is119NewChunk = ModuleManager.getModule(PaletteNewChunks.class)
             .isNewChunk(
-                event.chunk().getPos().x,
-                event.chunk().getPos().z,
-                event.chunk().getWorld().getRegistryKey()
+                chunk.getPos().x,
+                chunk.getPos().z,
+                chunk.getWorld().getRegistryKey()
             );
 
-        switch (chunkLoadMode.get())
+        boolean is112OldChunk = ModuleManager.getModule(OldChunks.class)
+            .isOldChunk(
+                chunk.getPos().x,
+                chunk.getPos().z,
+                chunk.getWorld().getRegistryKey()
+            );
+
+        if (!is119NewChunk)
         {
-            case Basic:
-                basicLoad(isNewChunk, event, event.seenChunk());
-                break;
-            case FullyLoad:
-                fullyLoad(isNewChunk, event, event.seenChunk());
-                break;
+            // add chunks to the list
+            Vec3d pos = chunk.getPos().getCenterAtY(0).toCenterPos();
+            while(trail.size() >= maxTrailLength.get())
+            {
+                trail.pollFirst();
+            }
+            trail.add(pos);
+
+            // get average pos
+            Vec3d averagePos = calculateAveragePosition(trail);
+
+            // set yaw to face average pos
+            targetYaw = (float) Rotations.getYaw(averagePos);
         }
     }
 
-    private enum FollowModes
+    private Vec3d calculateAveragePosition(ArrayDeque<Vec3d> positions)
     {
-        Basic,
-        FullyLoad,
+        double sumX = 0, sumZ = 0;
+        for (Vec3d pos : positions) {
+            sumX += pos.x;
+            sumZ += pos.z;
+        }
+        return new Vec3d(sumX / positions.size(), 0, sumZ / positions.size());
+    }
+
+    private float smoothRotation(double current, double target)
+    {
+        double difference = angleDifference(target, current);
+        return (float) (current + difference * rotateScaling.get());
+    }
+
+    private double angleDifference(double target, double current)
+    {
+        double diff = (target - current + 180) % 360 - 180;
+        return diff < -180 ? diff + 360 : diff;
     }
 
 }
